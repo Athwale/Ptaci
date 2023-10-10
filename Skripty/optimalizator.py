f7bdbb891aa3510310ab43058f2e517f4668cc18
Ondrej Mejzlik <omejzlik@redhat.com>
2023-10-10 15:14:42 +0200

    Add optimize function

--- Skripty/optimalizator.py
+++ Skripty/optimalizator.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+import os
 from pathlib import Path
 from PIL import Image
 
@@ -10,6 +11,7 @@ def optimize_image(img_path: Path, thumbnail: int) -> None:
     :param thumbnail: thumbnail size.
     :return: None
     """
+    print(f'Optimizing: {img_path}')
     # Convert.
     if img_path.suffix.lower() == '.png':
         png = Image.open(img_path)
@@ -23,3 +25,21 @@ def optimize_image(img_path: Path, thumbnail: int) -> None:
     img = Image.open(img_path)
     img.thumbnail((thumbnail, thumbnail), Image.LANCZOS)
     img.save(img_path, "JPEG")
+
+
+if __name__ == '__main__':
+    working_directory = Path(Path.home() / 'Downloads/birds')
+    if not working_directory.exists():
+        print(f'Working directory not found: {working_directory}')
+    else:
+        os.chdir(working_directory)
+        for path in Path(Path.cwd()).iterdir():
+            if path.is_dir():
+                try:
+                    os.chdir(path)
+                    for img_p in list(Path.cwd().glob('*.jpg')):
+                        optimize_image(img_p, 500)
+                except:
+                    print(f'Error: {path}')
+                finally:
+                    os.chdir(working_directory)
