2c04c8e670c835320619da5a1c8071026ebe024e
Ondrej Mejzlik <omejzlik@redhat.com>
2023-10-12 19:23:35 +0200

    Collect colors

--- Skripty/vyplnovac.py
+++ Skripty/vyplnovac.py
@@ -1,39 +1,45 @@
 #!/bin/python3
 
 import tkinter as tk
+import yaml
+import os
+from tkinter import ttk
+from pathlib import Path
 
-if __name__ == '__main__':
-    # todo typ: papousek, dravec, sova, vodni, morsky, pevec
-    window = tk.Tk()
-    radio_var = tk.IntVar(window, 0)
-    check_var = tk.IntVar(window, 2)
-    radio_var_1 = tk.IntVar(window, 1)
+
+def create_gui():
+    """
+
+    :return:
+    """
+# todo typ: papousek, dravec, sova, vodni, morsky, pevec
+    root = tk.Tk()
+    root.geometry('300x200')
+    # Frames:
+    head_color = ttk.LabelFrame(root, text='Hlava')
+
+    radio_var = tk.IntVar(root, 0)
+    check_var = tk.IntVar(root, 2)
+    radio_var_1 = tk.IntVar(root, 1)
 
     label = tk.Label(text="Database filler")
-    label.pack()
 
-    R1 = tk.Radiobutton(window, text="Option 1", value=1, variable=radio_var)
+    R1 = tk.Radiobutton(head_color, text="Option 1", value=1, variable=radio_var)
     R1.pack()
-    R2 = tk.Radiobutton(window, text="Option 2", value=2, variable=radio_var)
+    R2 = tk.Radiobutton(head_color, text="Option 2", value=2, variable=radio_var)
     R2.pack()
-    R3 = tk.Radiobutton(window, text="Option 3", value=3, variable=radio_var)
+    R3 = tk.Radiobutton(head_color, text="Option 3", value=3, variable=radio_var)
     R3.pack()
 
-    R4 = tk.Radiobutton(window, text="Option 4", value=4, variable=radio_var_1)
-    R4.pack()
-    R5 = tk.Radiobutton(window, text="Option 5", value=5, variable=radio_var_1)
-    R5.pack()
+    R4 = tk.Radiobutton(root, text="Option 4", value=4, variable=radio_var_1)
+    R5 = tk.Radiobutton(root, text="Option 5", value=5, variable=radio_var_1)
 
-    c1 = tk.Checkbutton(window, text='Python', variable=check_var, onvalue=1, offvalue=0)
-    c1.pack()
+    c1 = tk.Checkbutton(root, text='Python', variable=check_var, onvalue=1, offvalue=0)
 
     button = tk.Button(text="Save", bg="green")
-    button.pack()
 
-    label = tk.Label(text="New color")
+    label1 = tk.Label(text="New color")
     entry = tk.Entry(width=50)
-    label.pack()
-    entry.pack()
 
     # TODO fill colors and attributes for each gender.
     # TODO add picture from wiki url, automatic rename.
@@ -42,4 +48,58 @@ if __name__ == '__main__':
     # TODO add dodatek.
     # TODO automatic image resize and make file smaller
 
-    window.mainloop()
+    head_color.pack()
+    root.mainloop()
+
+
+def analyze_yaml():
+    """
+    Get typ, velikost, barvy from both samec and samice.
+    :return:
+    """
+    color_dictionary = {'hlava': set(),
+                        'křídla': set(),
+                        'hruď': set(),
+                        'ocas': set(),
+                        'nohy': set(),
+                        'záda': set(),
+                        'zobák': set()}
+    with open('data.yml', 'r') as file:
+        metadata = yaml.safe_load(file)
+        print(metadata['typ'])
+        print(metadata['velikost'])
+        available_genders = ['samec']
+        if metadata['popis']['samice']:
+            available_genders.append('samice')
+        for gender in available_genders:
+            parts = metadata['popis'][gender]['barvy']
+            for body_part in parts:
+                key = list(body_part.keys())[0]
+                color_set = color_dictionary[key]
+                colors = set(body_part[list(body_part.keys())[0]])
+                new_colors = color_set.union(colors)
+                color_dictionary[key] = new_colors
+    print(color_dictionary)
+
+
+def scan_options():
+    """
+    Scan finished files to create options for the gui.
+    :return:
+    """
+    working_directory = Path(Path.cwd() / Path('../databaze/finished'))
+    os.chdir(working_directory)
+    for path in Path(Path.cwd()).iterdir():
+        if path.is_dir():
+            try:
+                os.chdir(path)
+                analyze_yaml()
+            except Exception as e:
+                print(f'Error: {path}, {e}')
+            finally:
+                os.chdir(working_directory)
+
+
+if __name__ == '__main__':
+    scan_options()
+    #create_gui()
