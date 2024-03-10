ac31e674d49e194a4484f2592a67d11eb52dbbc3
Ondrej Mejzlik <omejzlik@redhat.com>
2024-03-10 21:32:15 +0100

    Generate filter

--- Skripty/generator.py
+++ Skripty/generator.py
@@ -1,22 +1,54 @@
-import sys
+import os
 from pathlib import Path
 
-import yaml
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
+from vyplnovac import scan_options
+
+
+def fill_filter(index, filter_data) -> None:
+    """
+    Create the filter section in the template index.html
+    :param index: Parsed bs4 template html file.
+    :param filter_data: Data to create the filter elements from.
+    :return: None
+    """
+    translator = {'beak': 'zobák',
+                  'head': 'hlava',
+                  'chest': 'hruď',
+                  'wings': 'křídla',
+                  'back': 'záda',
+                  'tail': 'ocas',
+                  'legs': 'nohy',
+                  'size': 'velikost',
+                  'type': 'typ'}
+
+    for part in ('beak', 'head', 'chest', 'wings', 'back', 'tail', 'legs', 'size', 'type'):
+        html_part = index.find("div", {"id": part}).find('form')
+        for option in filter_data[translator[part]]:
+            option: str
+            #<input type="checkbox" id="červená" name="červená_h" value="červená"><label for="červená_h">červená</label><br>
+            part_id = f'{part}{option.capitalize()}'
+            input_item = index.new_tag('input', attrs={'type': 'checkbox', 'id': part_id, 'name': part_id,
+                                                       'value': option})
+            input_label = index.new_tag('label', attrs={'for': part_id})
+            input_item.append(input_label)
+            br = index.new_tag('br')
+            input_item.append(br)
+            input_label.insert(0, NavigableString(option))
+            html_part.append(input_item)
 
 
 if __name__ == '__main__':
+    www_dir = Path.cwd() / Path('../www')
     data_directory = Path(Path.cwd() / Path('../www/images/ptaci'))
-    if not data_directory.exists():
-        print(f'Working directory not found: {data_directory}')
-    else:
-        for path in data_directory.iterdir():
-            if path.is_dir():
-                try:
-                    metadata = list(path.glob('data.yml'))[0]
-                    with open(metadata, 'r', encoding='utf-8') as file:
-                        data = yaml.safe_load(file)
-                        print(data)
-                except Exception as e:
-                    print(f'Error: {path}, {e}')
-                    sys.exit(1)
+    with open(Path('./template.html'), 'r') as html:
+        template = BeautifulSoup(html, 'html.parser')
+        if not data_directory.exists():
+            print(f'Working directory not found: {data_directory}')
+        else:
+            fill_filter(template, scan_options(data_directory))
+
+            os.chdir(www_dir)
+            html = template.prettify("utf-8")
+            with open("index.html", "wb") as file:
+                file.write(html)
